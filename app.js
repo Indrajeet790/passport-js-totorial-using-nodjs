0d681c798d1096b34c990a0ba8c197974efffd22
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-03-10 10:51:57 +0530

    create route for login page

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const port = 8500;
 
+// This route for login page
+app.get("/login", (req, resp) => {
+  resp.send("login get");
+});
+
 app.listen(port, (err) => {
   if (err) {
     console.log("server is not valid");
