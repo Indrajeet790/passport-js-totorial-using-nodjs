6b0328798b1913beaa5ceb919a72b29e6d810ac3
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-03-12 07:57:13 +0530

    update code of schema

--- models/database.js
+++ models/database.js
@@ -1,10 +1,34 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+// const userSchema = new mongoose.Schema({
+//   username: String,
+//   password: String,
+// });
 
-const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
+// const userModel = new mongoose.model("user", userSchema);
+
+// var Schema = mongoose.Schema;
+// var UserDetail = new Schema(
+//   {
+//     username: String,
+//     password: String,
+//   },
+//   {
+//     collection: "userInfo",
+//   }
+// );
+
+// module.exports = userModel;
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+  username: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  //insert your other key of collection
 });
 
-const userModel = new mongoose.model("user", userSchema);
-
-module.exports = userModel;
+module.exports = mongoose.model("userModel", UserSchema);
