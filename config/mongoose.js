f18869fd8c00d80ba45c575aee47722e6f9bb15c
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-03-12 07:55:51 +0530

    updated code

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+
+// connection
 mongoose.connect("mongodb://127.0.0.1:27017/passport_js").then(() => {
   console.log("database successfully connected");
 });
